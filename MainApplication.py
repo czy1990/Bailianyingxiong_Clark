b80ce9e122217b3df92d2dbb9cc7bab48bf99aa5
Clark_Chen <chen_zyan@yeah.net>
2024-04-30 07:45:08 +0800

    自动打怪-61寂静山谷-开启

--- MainApplication.py
+++ MainApplication.py
@@ -1,5 +1,6 @@
 import MoveBoss as moveBoss
 import Utils as u
+import ClickItem as clickItem
 import MoveLevel as moveLevel
 import MoveDiamond as moveDiamond
  # 通过IP连接
@@ -27,27 +28,27 @@ def click升级4_1寒风营地():
     print("click升级4_1寒风营地")
     moveBoss.move41寒风营地UpLevel()
 
-
 count = 0
 while True:  # 这将创建一个无限循环
     count += 1
     print(count)
+
     moveBoss.move12教堂山谷Boss() # 近
     moveBoss.move21贫瘠营地Boss() # 近
-    # moveBoss.move31污染哨站Boss() # 近
+    moveBoss.move31污染哨站Boss() # 近
     # moveBoss.move32腐烂沼泽Boss1() # 远
     # moveBoss.move32腐烂沼泽Boss2() # 远
     moveBoss.move33寒风营地Boss() # 近
     # moveLevel.move41寒风营地UpLevel()
     moveLevel.move42寒风营地UpLevel_Diamond()
     moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
+    # moveLevel.move61寂静山谷UpLevel()
+    # moveLevel.move61寂静山谷UpLevel()
+    # moveLevel.move61寂静山谷UpLevel()
+    # moveLevel.move61寂静山谷UpLevel()
+    # moveLevel.move61寂静山谷UpLevel()
+    # moveLevel.move61寂静山谷UpLevel()
+    # moveLevel.move61寂静山谷UpLevel()
+    # moveLevel.move61寂静山谷UpLevel()
+    # moveLevel.move61寂静山谷UpLevel()
 
