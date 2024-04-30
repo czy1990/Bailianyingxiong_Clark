721a567ce37ad500b78fe3a38e54d3f222fcf683
Clark_Chen <chen_zyan@yeah.net>
2024-04-30 23:36:43 +0800

    自动打怪-61寂静山谷-增加严寒第三3个boss

--- MainApplication.py
+++ MainApplication.py
@@ -34,25 +34,26 @@ def click升级4_1寒风营地():
     moveBoss.move41寒风营地UpLevel()
 
 
+def getUpLevel():
+    count = 0
+    while True:  # 这将创建一个无限循环
+        count += 1
+        print(count)
+        #     moveLevel.move42寒风营地UpLevel_Diamond()
+        moveLevel.move61寂静山谷UpLevel()
+
 count = 0
 while True:  # 这将创建一个无限循环
     count += 1
     print(count)
+    moveBoss.move41魔力之环Boss1()  # 远
     moveBoss.move12教堂山谷Boss()  # 近
     moveBoss.move21贫瘠营地Boss()  # 近
+
+    moveBoss.move42北风营地Boss()  # 远
     moveBoss.move31污染哨站Boss()  # 近
-    # moveBoss.move32腐烂沼泽Boss1() # 远
-    # moveBoss.move32腐烂沼泽Boss2() # 远
+    moveBoss.move32腐烂沼泽Boss1()  # 远
+
+    moveBoss.move41魔力之环Boss1()  # 远
+    moveBoss.move32腐烂沼泽Boss2()  # 远
     moveBoss.move33寒风营地Boss()  # 近
-    # moveLevel.move41寒风营地UpLevel()
-    moveLevel.move42寒风营地UpLevel_Diamond()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
-    moveLevel.move61寂静山谷UpLevel()
